61bd7047112b9d9b8c46850cac078bda306487b5
Siriuslicht <laurairlean@gmail.com>
2025-07-22 20:31:13 +0500

    fix: develop screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,22 +2,27 @@ import React from "react";
 import "../css/app.css";
 import {Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch } from "react-router-dom";
-import { Users } from "./screens/Users";
-import { About } from "./screens/About";
+import { HomePage } from "./screens/homePage";
+import { ProductsPage } from "./screens/productsPage";
+import { OrdersPage } from "./screens/ordersPage";
+import { UsersPage } from "./screens/userPage";
 
 function App() {
   return (
     <div>
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
+           <li>
+              <Link to="/products">ProductsPage</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/products">ProductsPage</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">UsersPage</Link>
             </li>
           </ul>
         </nav>
@@ -25,22 +30,26 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/products">
+            <ProductsPage />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <UsersPage />
           </Route>
           <Route path="/">
-            <Home />
+            <HomePage />
           </Route>
         </Switch>
       </div>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
 export default App;
+
+
+// screens component 
+// sectional component
+// reusable component
