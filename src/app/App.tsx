e0fa3b28a813c8ae490cec3fb7f009be69d8c1cb
Siriuslicht <laurairlean@gmail.com>
2025-08-19 01:33:31 +0500

    fix: modify App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -38,7 +38,7 @@ function App() {
             <HelpPage />
           </Route>
           <Route path="/">
-            <HomePage />
+            <HomePage /> 
           </Route>
         </Switch>
           <Footer />
