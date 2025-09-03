85170969bb6984d2eaf527bbfcbf9248057be8c5
Siriuslicht <laurairlean@gmail.com>
2025-09-04 02:00:45 +0500

    fix: OrderService modified

---
+++ src/app/services/Order.service.ts
@@ -0,0 +1,14 @@
+import exios from "axios";
+import { serverApi } from "../../lib/config";
+
+class OrderService {
+   private readonly path: string;
+
+   constructor() {
+      this.path = serverApi;
+   }
+
+
+}
+
+export default OrderService;
